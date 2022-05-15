d2459f2efd9f3e28177fae6cc2ba0e66bcebc26d
gabe <ggetzie@gmail.com>
2022-05-15 08:11:40 +0800

    credits

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -6,18 +6,28 @@ const About = () => {
     <>
       <ScreenHeader showBack={true} title="About" />
       <div className="vert-center">
-        <h3>Concept and Content</h3>
-        <p>
+        <h3 className="mb-4">Concept and Content</h3>
+        <p className="m-0">
           <a href="https://msteinberg.art">Monica Steinberg</a>
         </p>
-        <h3>Programming</h3>
-        <p>
-          <a href="https://kotsf.com">Kotsf, LLC</a>
+
+        <h3 className="mb-4 mt-10">Programming</h3>
+        <p className="m-0">
+          <a href="https://kotsf.com">Kotsf, Limited</a>
         </p>
-        <h3>Design</h3>
-        <p>Taylor Lovell</p>
-        <h3>Illustrations</h3>
-        <p>Hoying "Pinky" Yuen</p>
+
+        <h3 className="mb-4 mt-10">Design</h3>
+        <p className="m-0">
+          <a href="https://taylorlovell.myportfolio.com/">Taylor Lovell</a>
+        </p>
+
+        <h3 className="mb-4 mt-10">Illustrations</h3>
+        <p className="m-0">Hoying "Pinky" Yuen</p>
+
+        <h3 className="mb-4 mt-10">Fonts</h3>
+        <p className="m-0">
+          <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+        </p>
       </div>
     </>
   );
